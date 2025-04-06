77e631c9fab0deaf00858533538b3f260422e81a
duong158 <translateman7@gmail.com>
2025-04-06 14:38:24 +0700

    tạo mới

---
+++ src/main/java/hoyocon/bomberman/HelloController.java
@@ -0,0 +1,14 @@
+package hoyocon.bomberman;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module hoyocon.bomberman {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires com.almasb.fxgl.all;
+
+    opens hoyocon.bomberman to javafx.fxml;
+    exports hoyocon.bomberman;
+}
