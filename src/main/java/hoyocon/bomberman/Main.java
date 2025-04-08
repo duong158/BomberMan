8fa99e440594bba7eac90884ebf780c0a3583969
Muaxuannamay <nguyenbaan2362006@gmail.com>
2025-04-08 13:54:28 +0700

    thêm file

--- src/main/java/hoyocon/bomberman/Main.java
+++ src/main/java/hoyocon/bomberman/Main.java
@@ -1,6 +1,11 @@
 package hoyocon.bomberman;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -9,7 +14,11 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Start-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
 }
