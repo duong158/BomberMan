2770853a5341ae62865a4885a6139b9751ffb2ed
Muaxuannamay <nguyenbaan2362006@gmail.com>
2025-05-04 21:52:17 +0700

    fix New Game

--- src/main/java/hoyocon/bomberman/Save/BombState.java
+++ src/main/java/hoyocon/bomberman/Save/BombState.java
@@ -5,5 +5,4 @@ import java.io.Serializable;
 public class BombState implements Serializable {
     public double x, y;          // Tọa độ pixel
     public long timeRemaining;   // Cho đến khi nổ
-    public long timePlaced;      // Mốc thời gian khi đặt
 }
